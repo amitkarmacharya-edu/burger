65a69d76311cf23659a0418f427667f82c687846
Amit Karmacharya <amitkarmacharya@Amits-MacBook-Pro.local>
2020-11-14 23:51:12 -0500

    created orm.js, can add, update and select from burgers table

---
+++ config/orm.js
@@ -0,0 +1,67 @@
+const con = require('./connection');
+
+const orm = {
+
+    selectAll: function() {
+        return new Promise(function(reject, resolve) {
+            con.query("SELECT * FROM burgers;", function(err, result) {
+                if(err) {
+                    console.log("Error while fetching data from burgers table");
+                    reject(err);
+                } 
+                // success
+                console.log("Received data from datablase, all rows");
+                resolve(result);
+            });
+        });
+    },
+
+    insertOne: function(burgerName, devoured) {
+        let queryString = "INSERT INTO burgers (burger_name, devoured) VALUES (??,??)";
+        return new Promise(function(reject, resolve) {
+            con.query(queryString, [burgerName, devoured], function(err, result){
+                if(err) {
+                    console.log("Err while inserting data into table");
+                    reject(err);
+                }
+
+                // success
+                resolve(result);
+            });
+        });
+    },
+
+    updateOne: function(burderName, devoured, id) {
+        if(!this.findOne(burderName, id)){
+            console.log("Burger NOT found!");
+            return false;
+        }
+        let queryString = "UPDATE burgers SET burger_name=??, devoured=?? WHERE id=??";
+        return new Promise(function(reject, resolve) {
+            if(err) {
+                console.log("Erro while updating burger info!", err);
+                reject(false);
+            }
+
+            // success
+            console.log("Successfully Updated Record");
+            resolve(true);
+        });
+    },
+
+    findOne: function(burgerName, id) {
+        let queryString = "SELECT * FROM burgers WHERE burger_name = ?? AND id = ??";
+        return new Promise(function(reject, resolve) {
+            con.query(queryString, [burgerName, id], function(err, result) {
+                if(err) {
+                    reject(false);
+                }
+                // success
+                resolve(true);
+            });
+        });
+    }
+
+};
+
+module.exports = orm;
