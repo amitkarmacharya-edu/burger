fe65450a59174ff6202e553834c9de71e48c894d
Amit Karmacharya <amitkarmacharya@Amits-MacBook-Pro.local>
2020-11-14 23:49:24 -0500

    created seeds.sql, inserted 3 records to burgers table

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- insert into burgers_db.burgers table
+INSERT INTO name(burgers_name, devoured) VALUES('crispy butter chicken', false);
+INSERT INTO name(burgers_name, devoured) VALUES('double layer burger', true);
+INSERT INTO name(burgers_name, devoured) VALUES('2xspicy burger', false);
