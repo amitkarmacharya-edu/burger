90b9da9134b02b151c095d32b8e44c168f4aff5b
Amit Karmacharya <amitkarmacharya@Amits-MacBook-Pro.local>
2020-11-20 12:02:34 -0500

    prototype-1: click and drag to devour complete

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,30 +1,26 @@
 $(document).ready(function() {
 
-
+  // draggable and droppable effect
   $('.draggable').sortable({connectWith: '.droppable'});
   $('.droppable').sortable({
     receive: function(event, ui){
-      
+
       let d = {
         burgerName: $(ui.item).find('span').text(),
         devoured: true,
         id: $(ui.item).find('span').attr('id')
       }
+
       ui.item.remove();
 
       $.ajax("/update/burger", {
         method: "PUT",
         data: d
       }).then(function(data, status) {
-
           // Reload the page to get the updated list
-          console.log("Is this working");
           location.reload();
-        }
-      );
+        });
     }
-
-    
   });
 
   // create burger
@@ -41,5 +37,6 @@ $(document).ready(function() {
     }
 
   });
+
 
 });
